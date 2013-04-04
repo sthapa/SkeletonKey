fd96f3b591f0cf35eceac41c33b5a6205a72088b
Suchandra Thapa <sthapa@ci.uchicago.edu>
2013-04-04 11:31:56 -0500

    Changes and fixes to SkeletonKey

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import glob,re, os
+
+
+
+def get_version():
+  """
+  Gets version from osg-configure script file
+  """
+  buffer = open('scripts/skeleton_key').read()
+  match = re.search("VERSION\s+=\s+'(.*)'", buffer)
+  return match.group(1)
+  
+setup(name='skeleton-key',
+      version=get_version(),
+      description='Package for skeleton-key and associated scripts',
+      author='Suchandra Thapa',
+      author_email='sthapa@ci.uchicago.edu',
+      url='http://sk.uchicago.edu',
+      scripts=['scripts/skeleton_key'],
+      data_files=['config/uc3-sample.ini']
+      )
+
